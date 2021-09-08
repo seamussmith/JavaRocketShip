081e88f3feffbe7194fc5b1c1a3818d7c38ab5fe
seamussmith <student.141504@worcesterschools.net>
2021-09-08 09:39:38 -0400

    Basic skeleton

--- src/main/App.java
+++ src/main/App.java
@@ -6,4 +6,27 @@ public class App
     {
         System.out.println("Hello, World!");
     }
+    static String buildCommandPod(int size)
+    {
+        return null;
+    }
+    static String buildSection(int size)
+    {
+        return null;
+    }
+    static String buildSectionReverse(int size)
+    {
+        return null;
+    }
+    static String buildSectionSeperator(int size)
+    {
+        return null;
+    }
+    static String buildRocket(int size)
+    {
+        var rocketAssembly = new StringBuilder();
+        rocketAssembly.append(buildCommandPod(size));
+        
+        return null;
+    }
 }
