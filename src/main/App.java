cd99bc1d7b159d999c5f0e092b3db73dfea3fbf6
seamussmith <student.141504@worcesterschools.net>
2021-09-08 19:06:53 -0400

    Accept user input

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,35 @@
 package main;
 
+import java.util.Scanner;
+
 import gov.nasa.rocketbuildingtools.VehicleAssemblyBay;
 
 public class App
 {
     public static void main(String[] args) 
     {
-        int s = 3;
+        var scan = new Scanner(System.in);
+        int s;
+
+        while (true)
+        {
+            try
+            {
+                System.out.print("What size rocket do you want? > ");
+                var input = scan.nextLine();
+                s = Integer.parseInt(input);
+                if (s <= 0)
+                {
+                    System.out.println("Please enter a number higher than zero");
+                    continue;
+                }
+                break;
+            }
+            catch (Exception e)
+            {
+                System.out.println("Please enter a valid number");
+            }
+        }
         System.out.print(VehicleAssemblyBay.buildRocket(s));
     }
 }
